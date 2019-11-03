4ff548f71576f8b8360ad6ffb143952076693e15
alberto-paparella <55706326+alberto-paparella@users.noreply.github.com>
2019-11-03 15:41:19 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -12,8 +12,8 @@
 
 //----- MACROS --------------------------------------------------------------
 #define Y 4         // numero servitori
-#define NRIP 100    // cicli
-#define SEED 5647    // Seme per la generazione di numeri pseudo-casuali
+#define PKTGEN 50  // pacchetti generati a ogni sperimentazione
+#define SEED 5647   // Seme per la generazione di numeri pseudo-casuali
                     // e per la ripetibilita' dell'esperimento
 
 //===== Main program ========================================================
@@ -21,12 +21,14 @@ int main(int argc, char* argv[]) {
 
     // Variabili
     int ii, t;              // Iteratori
-    int kGen = 1000;        // Pacchetti che verranno generati nel corso della simulazione
+    int kGen = PKTGEN;        // Pacchetti che verranno generati nel corso della simulazione
     int k = 0;              // Numero pacchetti nel sistema
     int kRif = 0;           // Pacchetti rifiutati dal sistema
     int nextArrivo = 0;     // Fra quanto tempo arrivera' il prossimo pacchetto
     int sommaK = 0;         // Utile per exp
+    int sommaKcoda = 0;
     double eK = 0.0;        // Utile per exp
+    double eKq = 0.0;
     double lambda;          // Tasso di nascita
     double mu;              // Tasso di morte
     double lmbk, muk;       // Parametri istantanei di lambda e mu
@@ -58,12 +60,13 @@ int main(int argc, char* argv[]) {
 
     fprintf(fp, "Andamento di E{K} in funzione di A\nA\t\tE{K}\n");
 
-    lambda = 0.1;
+    //lambda = 0.1; // Per il DEBUG commentando il ciclo
     mu = 0.01;
     for (lambda = 0.001; lambda < 0.2; lambda += 0.001) {
 
         sommaK = 0;
-        kGen = 100;
+        sommaKcoda = 0;
+        kGen = PKTGEN;
         kRif = 0;
         t = 0;  // per quanti cicli va la sim
         while (kGen) {
@@ -85,7 +88,9 @@ int main(int argc, char* argv[]) {
                 // printf("PKT nel sistema: %d\tProssimo PKT: %d\nStato Servitori:\n", k, nextArrivo);
                 // for (ii = 0; ii < Y; ii++)
                 //   printf("Servitore: %d\tSta servendo: %d\tOccupato: %d\tLibero fra: %d\nCoda:\n", ii+1, servitori[ii].pacchetto.id, servitori[ii].Occupato, servitori[ii].tServizio);
-                //stampaCoda(c1);
+                // stampaCoda(c1);
+                // printf("PKT in coda: %d\n", getStato(c1));
+                // printf("PKT scartati dall'inizio della simulazione: %d\n, kRif);
 
                 /* Gestione parametro lambda */
                 if (k < Y)
@@ -113,8 +118,8 @@ int main(int argc, char* argv[]) {
                         servitori[ii].pacchetto.id = c1->pacchetto.id;  // Copio l'indirizzo del pacchetto in testa alla coda
                                                                         // nel servitore; S ora sta servendo quel pkt
                         servitori[ii].Occupato = 1;
-                        muk = k * mu;   // Tanto mi serve solo qui
-                        servitori[ii].tServizio = poisson(muk);
+                        muk = k * mu;   // nb: muk e' a livello di sistema, non la uso nella simulazione
+                        servitori[ii].tServizio = poisson(mu);
                         dequeue(&c1);    // ATTENZIONE: in realta' prima viene effettuata la dequeue e poi il pacchetto
                                         // entra nel servitore, ovviamente. Cio' e' invertito per semplificare
                                         // l'implementazione della simulazione
@@ -130,16 +135,17 @@ int main(int argc, char* argv[]) {
                 }
 
                 sommaK += k;
+                sommaKcoda += getStato(c1);
                 t++;
 
         }
 
-        eK = (double)sommaK/(double)t;
+        eK = (double)sommaK / (double)t;
+        eKq = (double)sommaKcoda / (double)t;
         fprintf(fp, "%f\t%f\n", lambda/mu, eK);    // stampo i valori istantanei di A e E{K}
-
+        printf("A: %f\tE{K}: %f\tE{Kq}: %f\tPKT scartati: %d\n", lambda/mu, eK, eKq, kRif);
     }
 
-    //printf("Pacchetti scartati: %d\n", kRif);
     fclose(fp);
 
     return 0;
